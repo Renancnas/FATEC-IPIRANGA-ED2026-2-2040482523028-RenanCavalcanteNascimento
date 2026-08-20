b999d417c66928167ac1a910d378d1058b1d933b
Renan Cavalcante Nascimento <renancavalcante2020@gmail.com>
2026-08-20 13:15:27 -0300

    primeiro teste de commit

--- EDI-Atividade-N1/EDI-Atividade-N1-1/pgm-n11.c
+++ EDI-Atividade-N1/EDI-Atividade-N1-1/pgm-n11.c
@@ -1,2 +1,7 @@
-git sdmokasmd
-,asod
+
+// define tamanho da turma, como a primeira coisa que pede nas orientações do trabalho
+#define TAMANHO_TURMA 5
+
+// Funções
+void exibirCabecalho(void);
+float calcularMedia(float vetor[], int tamanho);
